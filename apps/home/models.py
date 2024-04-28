731f83cde7f7b2be5af737e03e7ff7b4e7078f37
Jmtrivinop <jcarrancho10@gmail.com>
2024-04-27 22:23:17 -0500

    Model Profesor Materia

--- apps/home/models.py
+++ apps/home/models.py
@@ -8,16 +8,19 @@ from django.contrib.auth.models import User
 
 # Create your models here
 
-class Estudiante(models.Model):
+class estudiante():
+    pass
 
+class Profesor(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
+    correo = models.EmailField(max_length=50)
     codigo = models.CharField(max_length=30)
-    correo = models.EmailField()
-    programa_academico = models.ForeignKey(Programa, on_delete=models.CASCADE, related_name='estudiantes')
-    materias = models.ManyToManyField(Materia, related_name='estudiantes')
-    usuario = models.CharField(max_length=20)
-    constrasena = models.CharField(max_length=20)
+    facultad = models.CharField(max_length=50)
+    usuario = models.CharField(max_length=50)
+    contrasena = models.CharField(max_length=50)
+    materias = models.ManyToManyField(Materia, related_name = 'profesor')
+
 
 class Materia(models.Model):
     nombreMateria = models.CharField(max_length=100) 
@@ -29,7 +32,3 @@ class Materia(models.Model):
     valoracion = models.CharField(max_length=100)
     puntuacion = models.CharField(max_length=100)
 
-class Valoracion(models.Model):
-    asunto = models.CharField(max_length=100)
-    comentario = models.CharField(max_length=100)
-    puntuacion = models.CharField(max_length=100)
