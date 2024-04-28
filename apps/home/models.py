18d3816e7b1374ee73ce88b3718ced56fbe49f13
aflasso <andresfelipe.lasso@gmail.com>
2024-04-27 21:59:49 -0500

    Estudiante model

--- apps/home/models.py
+++ apps/home/models.py
@@ -8,7 +8,13 @@ from django.contrib.auth.models import User
 
 # Create your models here
 
-class estudiante():
-    
-    
-    pass
+class Estudiante(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    codigo = models.CharField(max_length=30)
+    correo = models.EmailField()
+    programa_academico = models.ForeignKey(Programa, on_delete=models.CASCADE, related_name='estudiantes')
+    materias = models.ManyToManyField(Materia, related_name='estudiantes')
+    usuario = models.CharField(max_length=20)
+    constrasena = models.CharField(max_length=20)
